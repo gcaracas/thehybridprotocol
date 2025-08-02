58edf23b8e2c5296f296531fc71a69c331ff04e4
Gerardo Caracas <gerardo.caracas@gmail.com>
2025-08-02 13:15:02 -0600

    Update favicon

--- hybridprotocol-frontend/app/layout.js
+++ hybridprotocol-frontend/app/layout.js
@@ -44,6 +44,9 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en" className="no-mobile no-touch ">
       <head>
+        <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32.png" />
+        <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16.png" />
+        <link rel="apple-touch-icon" sizes="180x180" href="/favicon.png" />
         <link
           href="https://fonts.googleapis.com/css2?family=DM+Sans:wght@400;500;700&display=swap"
           rel="stylesheet"
